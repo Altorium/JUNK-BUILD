6763f88b062e81955428f6041949f15427f08cbc
Altorium <haluhiro.611@icloud.com>
2026-03-06 21:05:15 +0900

    Add files via upload

---
+++ Hackson/cards.js
@@ -0,0 +1,11 @@
+const card_definitions = {
+    "card_1": {
+        id:"cpu_1",
+        name:"Ryzen 7 9700x",
+        type:"cpu",
+        price:"40000",
+        score:"800",
+        
+    }
+};
+
