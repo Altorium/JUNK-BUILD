57abbc6a4ed6d3946f3822ee42fc023d2f00f6a5
yu <takeuchiyu6@gmail.com>
2026-03-07 14:51:42 +0900

    add cards.js file

--- Hackson/cards.js
+++ Hackson/cards.js
@@ -4,110 +4,486 @@ const cards = {
 
   cpu: [
     {
-      name: "Ryzen 5 5600X",
-      cost: 30000,
-      socket: "AM4",
-      score: 650,
+      name: "Gore I9",
+      cost: 60,
+      socket: "[G]",
+      score: 8000,
+      power: 250,
+      reliability: "new"
+    },
+    {
+      name: "Rizen 9",
+      cost: 60,
+      socket: "[R]",
+      score: 8000,
+      power: 200,
+      reliability: "new"
+    },
+    {
+      name: "Gore I7",
+      cost: 45,
+      socket: "[G]",
+      score: 6000,
+      power: 150,
+      reliability: "new"
+    },
+    {
+      name: "Rizen 7",
+      cost: 45,
+      socket: "[R]",
+      score: 6000,
+      power: 105,
+      reliability: "new"
+    },
+    {
+      name: "Gore I5",
+      cost: 30,
+      socket: "[G]",
+      score: 4000,
       power: 65,
       reliability: "new"
     },
     {
-      name: "Core i7 13700K",
-      cost: 50000,
-      socket: "LGA1700",
-      score: 850,
-      power: 125,
+      name: "Rizen 5",
+      cost: 30,
+      socket: "[R]",
+      score: 4000,
+      power: 65,
       reliability: "new"
     },
     {
-      name: "中古 Ryzen 7 3700X",
-      cost: 20000,
-      socket: "AM4",
-      score: 600,
+      name: "Gore I3",
+      cost: 15,
+      socket: "[G]",
+      score: 2000,
+      power: 35,
+      reliability: "new"
+    },
+    {
+      name: "Rizen 3",
+      cost: 15,
+      socket: "[R]",
+      score: 2000,
+      power: 35,
+      reliability: "new"
+    },
+    {
+      name: "Gore I9",
+      cost: 42,
+      socket: "[G]",
+      score: 8000,
+      power: 250,
+      reliability: "used"
+    },
+    {
+      name: "Rizen 9",
+      cost: 42,
+      socket: "[R]",
+      score: 8000,
+      power: 200,
+      reliability: "used"
+    },
+    {
+      name: "Gore I7",
+      cost: 32,
+      socket: "[G]",
+      score: 6000,
+      power: 150,
+      reliability: "used"
+    },
+    {
+      name: "Rizen 7",
+      cost: 32,
+      socket: "[R]",
+      score: 6000,
+      power: 105,
+      reliability: "used"
+    },
+    {
+      name: "Gore I5",
+      cost: 21,
+      socket: "[G]",
+      score: 4000,
       power: 65,
       reliability: "used"
+    },
+    {
+      name: "Rizen 5",
+      cost: 21,
+      socket: "[R]",
+      score: 4000,
+      power: 65,
+      reliability: "used"
+    },
+    {
+      name: "Gore I3",
+      cost: 11,
+      socket: "[G]",
+      score: 2000,
+      power: 35,
+      reliability: "used"
+    },
+    {
+      name: "Rizen 3",
+      cost: 11,
+      socket: "[R]",
+      score: 2000,
+      power: 35,
+      reliability: "used"
+    },
+    {
+      name: "Gore I9",
+      cost: 18,
+      socket: "[G]",
+      score: 8000,
+      power: 250,
+      reliability: "junk"
+    },
+    {
+      name: "Rizen 9",
+      cost: 18,
+      socket: "[R]",
+      score: 8000,
+      power: 200,
+      reliability: "junk"
+    },
+    {
+      name: "Gore I7",
+      cost: 14,
+      socket: "[G]",
+      score: 6000,
+      power: 150,
+      reliability: "junk"
+    },
+    {
+      name: "Rizen 7",
+      cost: 14,
+      socket: "[R]",
+      score: 6000,
+      power: 105,
+      reliability: "junk"
+    },
+    {
+      name: "Gore I5",
+      cost: 9,
+      socket: "[G]",
+      score: 4000,
+      power: 65,
+      reliability: "junk"
+    },
+    {
+      name: "Rizen 5",
+      cost: 9,
+      socket: "[R]",
+      score: 4000,
+      power: 65,
+      reliability: "junk"
+    },
+    {
+      name: "Gore I3",
+      cost: 5,
+      socket: "[G]",
+      score: 2000,
+      power: 35,
+      reliability: "junk"
+    },
+    {
+      name: "Rizen 3",
+      cost: 5,
+      socket: "[R]",
+      score: 2000,
+      power: 35,
+      reliability: "junk"
     }
   ],
 
   gpu: [
     {
-      name: "RTX 4060",
-      cost: 45000,
-      score: 700,
-      power: 115,
+      name: "Z-Force 90",
+      cost: 80,
+      score: 11000,
+      power: 450,
       reliability: "new"
     },
     {
-      name: "RTX 3080（中古）",
-      cost: 50000,
-      score: 900,
-      power: 320,
+      name: "Radion 900",
+      cost: 70,
+      score: 9500,
+      power: 350,
+      reliability: "new"
+    },
+    {
+      name: "Z-Force 70",
+      cost: 50,
+      score: 7000,
+      power: 250,
+      reliability: "new"
+    },
+    {
+      name: "Radion 700",
+      cost: 45,
+      score: 6500,
+      power: 220,
+      reliability: "new"
+    },
+    {
+      name: "Z-Force 60",
+      cost: 35,
+      score: 4500,
+      power: 150,
+      reliability: "new"
+    },
+    {
+      name: "Radion 600",
+      cost: 30,
+      score: 4000,
+      power: 130,
+      reliability: "new"
+    },
+    {
+      name: "Z-Force 50",
+      cost: 10,
+      score: 1500,
+      power: 75,
+      reliability: "new"
+    },
+    {
+      name: "Radion 500",
+      cost: 10,
+      score: 1500,
+      power: 50,
+      reliability: "new"
+    },
+    {
+      name: "Z-Force 90",
+      cost: 56,
+      score: 11000,
+      power: 450,
       reliability: "used"
     },
     {
-      name: "GTX 1660",
-      cost: 25000,
-      score: 450,
-      power: 120,
-      reliability: "new"
+      name: "Radion 900",
+      cost: 49,
+      score: 9500,
+      power: 350,
+      reliability: "used"
+    },
+    {
+      name: "Z-Force 70",
+      cost: 35,
+      score: 7000,
+      power: 250,
+      reliability: "used"
+    },
+    {
+      name: "Radion 700",
+      cost: 32,
+      score: 6500,
+      power: 220,
+      reliability: "used"
+    },
+    {
+      name: "Z-Force 60",
+      cost: 25,
+      score: 4500,
+      power: 150,
+      reliability: "used"
+    },
+    {
+      name: "Radion 600",
+      cost: 21,
+      score: 4000,
+      power: 130,
+      reliability: "used"
+    },
+    {
+      name: "Z-Force 50",
+      cost: 7,
+      score: 1500,
+      power: 75,
+      reliability: "used"
+    },
+    {
+      name: "Radion 500",
+      cost: 7,
+      score: 1500,
+      power: 50,
+      reliability: "used"
+    },
+    {
+      name: "Z-Force 90",
+      cost: 24,
+      score: 11000,
+      power: 450,
+      reliability: "junk"
+    },
+    {
+      name: "Radion 900",
+      cost: 21,
+      score: 9500,
+      power: 350,
+      reliability: "junk"
+    },
+    {
+      name: "Z-Force 70",
+      cost: 15,
+      score: 7000,
+      power: 250,
+      reliability: "junk"
+    },
+    {
+      name: "Radion 700",
+      cost: 14,
+      score: 6500,
+      power: 220,
+      reliability: "junk"
+    },
+    {
+      name: "Z-Force 60",
+      cost: 11,
+      score: 4500,
+      power: 150,
+      reliability: "junk"
+    },
+    {
+      name: "Radion 600",
+      cost: 9,
+      score: 4000,
+      power: 130,
+      reliability: "junk"
+    },
+    {
+      name: "Z-Force 50",
+      cost: 3,
+      score: 1500,
+      power: 75,
+      reliability: "junk"
+    },
+    {
+      name: "Radion 500",
+      cost: 3,
+      score: 1500,
+      power: 50,
+      reliability: "junk"
     }
   ],
 
   memory: [
     {
-      name: "16GB DDR4",
-      cost: 8000,
+      name: "メモリ for gen4 8G",
+      cost: 5,
+      memoryType: "DDR4",
+      capacity: 8,
+      reliability: "new"
+    },
+    {
+      name: "メモリ for gen4 16G",
+      cost: 10,
       memoryType: "DDR4",
       capacity: 16,
       reliability: "new"
     },
     {
-      name: "32GB DDR4",
-      cost: 15000,
+      name: "メモリ for gen4 32G",
+      cost: 20,
       memoryType: "DDR4",
       capacity: 32,
       reliability: "new"
     },
     {
-      name: "16GB DDR5",
-      cost: 12000,
+      name: "メモリ for gen5 8G",
+      cost: 10,
+      memoryType: "DDR5",
+      capacity: 8,
+      reliability: "new"
+    },
+    {
+      name: "メモリ for gen5 16G",
+      cost: 15,
       memoryType: "DDR5",
       capacity: 16,
-      reliability: "used"
+      reliability: "new"
+    },
+    {
+      name: "メモリ for gen5 32G",
+      cost: 25,
+      memoryType: "DDR5",
+      capacity: 32,
+      reliability: "new"
     }
   ],
 
   motherboard: [
     {
-      name: "B550",
-      cost: 15000,
-      socket: "AM4",
+      name: "Gore Z-Board",
+      cost: 25,
+      socket: "[G]",
+      memoryType: "DDR5",
+      reliability: "new"
+    },
+    {
+      name: "Rizen X-Board",
+      cost: 25,
+      socket: "[R]",
+      memoryType: "DDR5",
+      reliability: "new"
+    },
+    {
+      name: "Gore B-Board",
+      cost: 15,
+      socket: "[G]",
       memoryType: "DDR4",
       reliability: "new"
     },
     {
-      name: "Z690",
-      cost: 25000,
-      socket: "LGA1700",
-      memoryType: "DDR5",
+      name: "Rizen B-Board",
+      cost: 15,
+      socket: "[R]",
+      memoryType: "DDR4",
       reliability: "new"
     }
   ],
-
   psu: [
     {
-      name: "600W Bronze",
-      cost: 8000,
-      capacity: 600,
+      name: "1200W Platinum",
+      cost: 30,
+      capacity: 1200,
+      rating: "Platinum",
+      reliability: "new"
+    },
+    {
+      name: "850W Gold",
+      cost: 20,
+      capacity: 850,
+      rating: "Gold",
+      reliability: "new"
+    },
+    {
+      name: "650W Bronze",
+      cost: 10,
+      capacity: 650,
       rating: "Bronze",
       reliability: "new"
     },
     {
-      name: "750W Gold",
-      cost: 12000,
-      capacity: 750,
-      rating: "Gold",
+      name: "450W Standard",
+      cost: 5,
+      capacity: 450,
+      rating: "Standard",
       reliability: "new"
+    }
+  ],
+  support: [
+    {
+      name: "RGBケースファン",
+      cost: 10,
+      effect: "5%プラス" //サポカのここの書き方はどうする？パーセンテージ前提で「5」とか数字だけにする？
+
+    },
+    {
+      name: "謎のケースファン",
+      cost: 10,
+      effect: "さいころをふって1-3なら10%プラス，4-6なら10%マイナス"
     }
   ]
 };
